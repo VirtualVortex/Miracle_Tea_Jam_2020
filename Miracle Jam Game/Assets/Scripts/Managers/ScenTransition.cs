2d800663f21c108ace4da13c12f50e7377d31e0a
mo214885 <mo214885@falmouth.ac.uk>
2020-11-14 21:56:26 +0000

    Added scene transition script to the game.
    Signed-off-by: mo214885 <mo214885@falmouth.ac.uk>

---
+++ Miracle Jam Game/Assets/Scripts/Managers/ScenTransition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenTransition : MonoBehaviour
+{
+    [SerializeField]
+    string scene;
+    [SerializeField]
+    Animator anim;
+
+    public void PlayAnimation() => anim.enabled = true;
+
+    public void SwitchScene() => SceneManager.LoadScene(scene);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Contains("Player"))
+            SwitchScene();
+    }
+}
