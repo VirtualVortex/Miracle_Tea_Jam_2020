7a52e9686161b769979c88d01f560f0c6e923f91
mo214885 <mo214885@falmouth.ac.uk>
2020-11-14 21:02:42 +0000

    Added checkpoints to the game.
    Signed-off-by: mo214885 <mo214885@falmouth.ac.uk>

---
+++ Miracle Jam Game/Assets/Scripts/Player/CheckPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public Transform checkPoint;
+
+    private void MovetoCheckPoint() => transform.position = checkPoint.position + (Vector3.up * 1.5f);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Contains("Checkpoint"))
+            checkPoint = other.transform;
+
+        if (other.tag.Contains("Death"))
+            MovetoCheckPoint();
+    }
+}
